5389268efe93fca9c69160d901a859d8aec9fb16
Quentin <quentin.bidolet@gmail.com>
2023-12-06 15:29:44 +0100

    objet cours

---
+++ cours/07-objet/02-classe.js
@@ -0,0 +1,37 @@
+// class a été introduit avec ES6
+
+class Personne {
+    // Le constructeur : fonction pour initialiser un objet.
+    constructor(nom, prenom="Toto", age=30) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    sePresenter() {
+        console.log("Bonjour, je m'appelle " + this.nom + " " + this.prenom + " et j'ai " + this.age + " ans.");
+    }
+}
+
+const johnDoe = new Personne("DOE");
+console.log(johnDoe);
+johnDoe.sePresenter();
+
+
+// extends pour hériter
+class Employe extends Personne {
+    constructor(nom, prenom, age, poste) {
+        super(nom, prenom, age);
+        this.poste = poste;
+    }
+
+    travailler(){
+        // ...
+    }
+}
+
+const smith = new Employe("SMITH", "John", 25, "Informaticien");
+console.log(smith);
+smith.sePresenter();
+
+
